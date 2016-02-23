9da92f23d187f241ac912a23c2c3ba172dffee68
solomon121314 <karimi79@chmail.ir>
2016-02-23 14:03:19 +0330

    Create telepatch.lua

---
+++ plugins/telepatch.lua
@@ -0,0 +1,42 @@
+do
+
+function run(msg, matches)
+  return 'Telegram Bot '.. VERSION  .. [[ 
+ 
+  TelePatch V4 - close Source
+  
+  An advance Administration bot based on Woesdo/Telepatch 
+  
+  open git : https://github.com/solomon1*****/***TelePatch.git
+  
+  ♻️TelePatch♻️ PowerFull
+  
+ We protect your group to hackers or spammer
+ 
+ Our Admins : 
+ 
+ @WoesDo , @HanozamHamonam
+ 
+ special thanks to :
+ 
+ Teamus
+ 
+ Nod32
+ 
+ And the others
+ 
+ Our Channel : @SmokeUnion
+ 
+ All rights reserved.]]
+end
+
+return {
+  description = "Shows bot telepatch", 
+  usage = "تله پتچ: Shows bot telepatch",
+  patterns = {
+    "^تله پتچ$"
+  }, 
+  run = run 
+}
+
+end
