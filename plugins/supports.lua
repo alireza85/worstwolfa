49539d2c0732242cc98fd78b7b1184ba2c203b34
solomon121314 <karimi79@chmail.ir>
2016-02-23 14:09:44 +0330

    Create supports.lua

---
+++ plugins/supports.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید. join 111849670!"
+  end
+return {
+  description = "shows persian support", 
+  usage = "ساپورت : Return persian supports link",
+  patterns = {
+    "^ساپورت$",
+    "^ساپورت$",
+    "^ساپورت$",
+    "^>ساپورت",
+  },
+  run = run
+}
+end
