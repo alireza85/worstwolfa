a88537fca41234738ae88386a608a4e1d34bc764
solomon121314 <karimi79@chmail.ir>
2016-02-27 15:15:11 +0330

    Create add.lua

---
+++ plugins/add.lua
@@ -0,0 +1,35 @@
+ if matches[1] == 'add' and not matches[2] then
+    if is_realm(msg) then
+       return 'Error: Already a realm.'
+    end
+    print("group "..msg.to.print_name.."("..msg.to.id..") added")
+    return modadd(msg)
+  end
+   if matches[1] == 'add' and matches[2] == 'realm' then
+    if is_group(msg) then
+       return 'Error: Already a group.'
+    end
+    print("group "..msg.to.print_name.."("..msg.to.id..") added as a realm")
+    return realmadd(msg)
+  end
+  if matches[1] == 'rem' and not matches[2] then
+    print("group "..msg.to.print_name.."("..msg.to.id..") removed")
+    return modrem(msg)
+  end
+  if matches[1] == 'rem' and matches[2] == 'realm' then
+    print("group "..msg.to.print_name.."("..msg.to.id..") removed as a realm")
+    return realmrem(msg)
+  end
+  if matches[1] == 'chat_created' and msg.from.id == 0 and group_type == "group" then
+    return automodadd(msg)
+  end
+  if matches[1] == 'chat_created' and msg.from.id == 0 and group_type == "realm" then
+    return autorealmadd(msg)
+  end
+
+  if msg.to.id and data[tostring(msg.to.id)] then
+    local settings = data[tostring(msg.to.id)]['settings']
+    if matches[1] == 'chat_add_user' then
+      if not msg.service then
+        return "Are you trying to troll me?"
+      end
