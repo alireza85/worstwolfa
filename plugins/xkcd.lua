fd655112d8125ab54cbedc76e09725aff82bc7bb
solomon121314 <karimi79@chmail.ir>
2016-02-29 02:43:27 +0330

    Update xkcd.lua

--- plugins/xkcd.lua
+++ plugins/xkcd.lua
@@ -34,7 +34,7 @@ end
 
 function run(msg, matches)
   local receiver = get_receiver(msg)
-  if matches[1] == "!xkcd" then
+  if matches[1] == "ایکس ایگرگ" then
     url, title, alt = get_xkcd_random()
   else
     url, title, alt = get_xkcd(matches[1])
@@ -48,8 +48,8 @@ return {
   description = "Send comic images from xkcd",
   usage = {"!xkcd (id): Send an xkcd image and title. If not id, send a random one"},
   patterns = {
-    "^!xkcd$",
-    "^!xkcd (%d+)",
+    "^ایکس ایگرگ$",
+    "^ایکس ایگرگ (%d+)",
     "xkcd.com/(%d+)"
   },
   run = run
