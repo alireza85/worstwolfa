31792371a25a3f4e7240dbac32fbfb473aa162d5
solomon121314 <karimi79@chmail.ir>
2016-02-29 02:13:36 +0330

    Update set.lua

--- plugins/set.lua
+++ plugins/set.lua
@@ -13,24 +13,19 @@ local function save_value(msg, name, value)
 end
 local function run(msg, matches)
   if not is_momod(msg) then
-    return "For moderators only!"
+    return "فقط برای مدیر!"
   end
   local name = string.sub(matches[1], 1, 50)
   local value = string.sub(matches[2], 1, 1000)
   local name1 = user_print_name(msg.from)
-  savelog(msg.to.id, name1.." ["..msg.from.id.."] saved ["..name.."] as > "..value )
+  savelog(msg.to.id, name1.." ["..msg.from.id.."] ذخیره شد ["..name.."] به شماره > "..value )
   local text = save_value(msg, name, value)
   return text
 end
 
 return {
   patterns = {
-   "^[]ذخیره ([^%s]+) (.+)$"
+   "^ذخیره ([^%s]+) (.+)$"
   }, 
   run = run 
 }
-
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
