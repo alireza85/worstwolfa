0aba893f6725e2d13a1ac9d893d5cf8d2bf29b58
solomon121314 <karimi79@chmail.ir>
2016-02-29 01:59:23 +0330

    Update roll.lua

--- plugins/roll.lua
+++ plugins/roll.lua
@@ -1,6 +1,6 @@
 do
 
-local ROLL_USAGE = "!roll d<sides>|<count> d<sides>"
+local ROLL_USAGE = "شانسی d<sides>|<count> d<sides>"
 local DEFAULT_SIDES = 100
 local DEFAULT_NUMBER_OF_DICE = 1
 local MAX_NUMBER_OF_DICE = 100
@@ -10,7 +10,7 @@ function run(msg, matches)
   local sides = DEFAULT_SIDES
   local number_of_dice = DEFAULT_NUMBER_OF_DICE
 
-  if str:find("!roll%s+d%d+%s*$") then
+  if str:find("شانسی%s+d%d+%s*$") then
     sides = tonumber(str:match("[^d]%d+%s*$"))
   elseif str:find("!roll%s+%d+d%d+%s*$") then
     number_of_dice = tonumber(str:match("%s+%d+"))
@@ -29,14 +29,14 @@ function run(msg, matches)
     results = string.format(fmt, results, math.random(sides))
   end
 
-  return string.format("Rolling %dd%d:\n%s", number_of_dice, sides, results)
+  return string.format("شانسی %dd%d:\n%s", number_of_dice, sides, results)
 end
 
 return {
   description = "Roll some dice!",
   usage = ROLL_USAGE,
   patterns = {
-    "^!roll%s*.*"
+    "^شانسی%s*.*"
   },
   run = run
 }
