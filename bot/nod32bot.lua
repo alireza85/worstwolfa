49c9ea12e398442bfb758de800a0e5cc9c086dcd
solomon121314 <karimi79@chmail.ir>
2016-03-03 09:56:12 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -440,25 +440,6 @@ sodu : @behrooZyaghi
 
 👾👾👽👽👽🎭🎭🕹🕹💀💀👺👺👹👹
 
-کنترول ربات
-1- تله پتچ ✅
-2- سی پی یو 👾
-3- ورژن 👽
-4- ساپورت 🏁
-5- ساپورتس ➿
-6- !bot on 👽
-7 -!bot off 💀
-8- !setbotphoto 🗻
-9- !contactlist ✌🏻
-10- !dialoglist 🗣
-11- !delcontact 🆔
-12- !whois ⁉️
-
-🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿
-
-Designer🎨 : The Worst😍
-
-
 ]],
     help_text = [[
 ⚜TelePatch⚜ دستورات ربات :
