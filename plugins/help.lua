000dd8d7f3d8f8cff808affbede7f5e6e349c859
solomon121314 <karimi79@chmail.ir>
2016-03-03 12:11:57 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -142,33 +142,7 @@ function run(msg, matches)
 18- زمان (مکان) 📠
 19- شات از سفحه 🗾
 20- اخبار هکران 👺😈
-
-\n
-\n
-\n
-\n
-\n
-\n
-\n
-👾👾👽👽👽🎭🎭🕹🕹💀💀👺👺👹👹
-
-کنترول ربات
-1- تله پتچ ✅
-2- سی پی یو 👾
-3- ورژن 👽
-4- ساپورت 🏁
-5- ساپورتس ➿
-6- !bot on 👽
-7 -!bot off 💀
-8- !setbotphoto 🗻
-9- !contactlist ✌🏻
-10- !dialoglist 🗣
-11- !delcontact 🆔
-12- !whois ⁉️
-
-🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿🌿
-
-Designer🎨 : The Worst😍.]]
+]]
 end
 
 return {
