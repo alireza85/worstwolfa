a573cf029e13059f722bceef6765a61637072892
solomon121314 <karimi79@chmail.ir>
2016-03-11 23:54:34 +0330

    Create tosupport.lua

---
+++ plugins/tosupport.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return " لینک ساپورت ربات : \n https://telegram.me/joinchat/CQYGmwaqsMaVkEIhSSA2_Q"
+  end
+return {
+  description = "shows support link", 
+  usage = "!tosupport : Return supports link",
+  patterns = {
+    "^(لینک ساپورت ربات)$",
+    "^(لینک ساپورت ربات)$"
+  },
+  run = run
+}
+end
