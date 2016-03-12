1496670990e92be70f7aac1dd8702dd412d1c2c7
solomon121314 <karimi79@chmail.ir>
2016-03-13 02:11:08 +0330

    Update and rename fosh1.lua to fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "کیرم پس کلت کیری کس عمت عمتو گاییدم کونی تو از اون هاچای روزگاری کیر بابام تو کون عمت بره جاکش کله کیری  تو خیلی کونکشی عرفان" .. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "سلام کن به [name]",
+  patterns = {
+    "^فحش بده به (.*)$",
+    "^فحش بده به (.*)$"
+  }, 
+  run = run 
+}
+
+end
