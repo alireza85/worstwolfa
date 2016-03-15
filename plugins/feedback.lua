604d28fc5ba9dd7b545185ee4f1906f820d6a0f4
TheWorst1 <woesdo@chmail.ir>
2016-03-15 13:06:52 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,31 +1,17 @@
 do
  function run(msg, matches)
- 
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
+ local fuse = 'پیام جدیدی دریافت شد !!\n\nنام کاربریش: @''\n\nآی دی کاربر : ' .. msg.from.id .. '\n\nای دی گروه : ' .. msg.to.id .. '\n\nاسم کاربر: ' .. msg.from.print_name .. '\n\nنظر:\n\n'  .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
- 
- 
-   local text = matches[1]
-   local chat = "chat#id"..158990680 
-   --like : local chat = "chat#id"..12345678
-   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'بازخورد شما ارسال شد'
- 
+ local text = matches[1]
+ local sends = send_msg('user#id118682430', fuse, ok_cb, false)
+ return 'نظرات شما به درستی ارسال شد'
  end
- end
- return {
-  
-  description = "Feedback",
- 
-  usage = "!feedback message",
-  patterns = {
-  "^[!/][Ff]eedback (.*)$"
- 
-  },
-  run = run
- }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+end
+return {
+description = "Feedback",
+usage = "feedback message",
+patterns = {
+ "^(ارسال نظر)(.*)$"
+ },
+ run = run
+}
