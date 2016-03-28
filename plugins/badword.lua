2cafac9fee6ed2b0df86448c9274f445beb406e1
TheWorst1 <woesdo@chmail.ir>
2016-03-29 02:46:53 +0430

    Update badword.lua

--- plugins/badword.lua
+++ plugins/badword.lua
@@ -6,114 +6,95 @@
 #      
 
 ]]
-local function addword(msg, name)
-    local hash = 'chat:'..msg.to.id..':badword'
-    redis:hset(hash, name, 'newword')
-    return "کلمه جدید به فیلتر کلمات اضافه شد\n>"..name
+do
+function run_bash(str)
+    local cmd = io.popen(str)
+    local result = cmd:read('*all')
+    return result
 end
-
-local function get_variables_hash(msg)
-
-    return 'chat:'..msg.to.id..':badword'
-
-end 
-
-local function list_variablesbad(msg)
-  local hash = get_variables_hash(msg)
-
-  if hash then
-    local names = redis:hkeys(hash)
-    local text = 'لیست کلمات غیرمجاز :\n\n'
-    for i=1, #names do
-      text = text..'> '..names[i]..'\n'
-    end
-    return text
-	else
-	return 
+local api_key = nil
+local base_api = "https://maps.googleapis.com/maps/api"
+function get_latlong(area)
+  local api      = base_api .. "/geocode/json?"
+  local parameters = "address=".. (URL.escape(area) or "")
+  if api_key ~= nil then
+    parameters = parameters .. "&key="..api_key
+  end
+  local res, code = https.request(api..parameters)
+  if code ~=200 then return nil  end
+  local data = json:decode(res)
+ 
+  if (data.status == "ZERO_RESULTS") then
+    return nil
+  end
+  if (data.status == "OK") then
+    lat  = data.results[1].geometry.location.lat
+    lng  = data.results[1].geometry.location.lng
+    acc  = data.results[1].geometry.location_type
+    types= data.results[1].types
+    return lat,lng,acc,types
   end
 end
+function get_staticmap(area)
+  local api        = base_api .. "/staticmap?"
+  local lat,lng,acc,types = get_latlong(area)
 
-function clear_commandbad(msg, var_name)
-  --Save on redis  
-  local hash = get_variables_hash(msg)
-  redis:del(hash, var_name)
-  return 'پاک شدند'
+  local scale = types[1]
+  if     scale=="locality" then zoom=8
+  elseif scale=="country"  then zoom=4
+  else zoom = 13 end
+    
+  local parameters =
+    "size=600x300" ..
+    "&zoom="  .. zoom ..
+    "&center=" .. URL.escape(area) ..
+    "&markers=color:red"..URL.escape("|"..area)
+
+  if api_key ~=nil and api_key ~= "" then
+    parameters = parameters .. "&key="..api_key
+  end
+  return lat, lng, api..parameters
 end
 
-local function list_variables2(msg, value)
-  local hash = get_variables_hash(msg)
-  
-  if hash then
-    local names = redis:hkeys(hash)
-    local text = ''
-    for i=1, #names do
-	if string.match(value, names[i]) and not is_momod(msg) then
-	if msg.to.type == 'channel' then
-	delete_msg(msg.id,ok_cb,false)
-	else
-	kick_user(msg.from.id, msg.to.id)
 
+function run(msg, matches)
+	local hash = 'usecommands:'..msg.from.id..':'..msg.to.id
+	redis:incr(hash)
+	local receiver	= get_receiver(msg)
+	local city = matches[1]
+	if matches[1] == 'praytime' then
+	city = 'Tehran'
 	end
-return 
-end
-      --text = text..names[i]..'\n'
-    end
-  end
-end
-local function get_valuebad(msg, var_name)
-  local hash = get_variables_hash(msg)
-  if hash then
-    local value = redis:hget(hash, var_name)
-    if not value then
-      return
-    else
-      return value
-    end
-  end
-end
-function clear_commandsbad(msg, cmd_name)
-  --Save on redis  
-  local hash = get_variables_hash(msg)
-  redis:hdel(hash, cmd_name)
-  return ''..cmd_name..'  پاک شد'
-end
+	local lat,lng,url	= get_staticmap(city)
 
-local function run(msg, matches)
-  if matches[2] == 'addword' then
-  if not is_momod(msg) then
-   return 'only for moderators'
-  end
-  local name = string.sub(matches[3], 1, 50)
-
-  local text = addword(msg, name)
-  return text
-  end
-  if matches[2] == 'badwords' then
-  return list_variablesbad(msg)
-  elseif matches[2] == 'clearbadwords' then
-if not is_momod(msg) then return '_|_' end
-  local asd = '1'
-    return clear_commandbad(msg, asd)
-  elseif matches[2] == 'remword' or matches[2] == 'rw' then
-   if not is_momod(msg) then return '_|_' end
-    return clear_commandsbad(msg, matches[3])
-  else
-    local name = user_print_name(msg.from)
-  
-    return list_variables2(msg, matches[1])
-  end
+	local dumptime = run_bash('date +%s')
+	local code = http.request('http://api.aladhan.com/timings/'..dumptime..'?latitude='..lat..'&longitude='..lng..'&timezonestring=Asia/Tehran&method=7')
+	local jdat = json:decode(code)
+	local data = jdat.data.timings
+	local text = 'شهر: '..city
+	  text = text..'\nاذان صبح: '..data.Fajr
+	  text = text..'\nطلوع آفتاب: '..data.Sunrise
+	  text = text..'\nاذان ظهر: '..data.Dhuhr
+	  text = text..'\nغروب آفتاب: '..data.Sunset
+	  text = text..'\nاذان مغرب: '..data.Maghrib
+	  text = text..'\nعشاء : '..data.Isha
+	  text = text..'\n\n@GPMod Team'
+	if string.match(text, '0') then text = string.gsub(text, '0', '۰') end
+	if string.match(text, '1') then text = string.gsub(text, '1', '۱') end
+	if string.match(text, '2') then text = string.gsub(text, '2', '۲') end
+	if string.match(text, '3') then text = string.gsub(text, '3', '۳') end
+	if string.match(text, '4') then text = string.gsub(text, '4', '۴') end
+	if string.match(text, '5') then text = string.gsub(text, '5', '۵') end 
+	if string.match(text, '6') then text = string.gsub(text, '6', '۶') end
+	if string.match(text, '7') then text = string.gsub(text, '7', '۷') end
+	if string.match(text, '8') then text = string.gsub(text, '8', '۸') end
+	if string.match(text, '9') then text = string.gsub(text, '9', '۹') end
+	return text
 end
 
 return {
-  patterns = {
-  "^([!/])(rw) (.*)$",
-  "^([!/])(addword) (.*)$",
-   "^([!/])(remword) (.*)$",
-    "^([!/])(badwords)$",
-    "^([!/])(clearbadwords)$",
-"^(.+)$",
-	   
-  },
-  run = run
+  patterns = {"^[/!][Pp]raytime (.*)$","^[/!](praytime)$"}, 
+  run = run 
 }
 
+end
